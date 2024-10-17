f2e4023c040ed403f44c05e3b376842427d2fe17
Leo Gimenez <leogimenez.dev@gmail.com>
2024-10-17 11:09:29 -0300

    se trabaja en en funcion para cargar excel

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ def cargar_excel():
      archivo_path = filedialog.askopenfilename(
         filetypes=[("Archivo Excel", "*.xlsx *.xls")]
     )
+     if archivo_path:
+        try:
+            # Cargar datos del archivo Excel
+            df = pd.read_excel(archivo_path)
+            mostrar_datos(df)
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo leer el archivo: {str(e)}")
